ddc0531a631fdb5e3436b1400c5bc6565e17374a
Karim Radhouani <medkarimrdi@gmail.com>
2023-08-29 22:55:48 -0700

    apply processors after adding deleted paths

--- formatters/event_test.go
+++ formatters/event_test.go
@@ -303,7 +303,7 @@ func TestTagsFromGNMIPath(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := TagsFromGNMIPath(tt.args.p)
+			got, got1 := tagsFromGNMIPath(tt.args.p)
 			if got != tt.want {
 				t.Errorf("TagsFromGNMIPath() got = %v, want %v", got, tt.want)
 			}
