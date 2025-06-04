9b2bfd4b9be83b70804ec3758ccb9f222e781eb3
Karim Radhouani <medkarimrdi@gmail.com>
2025-06-04 08:54:03 -0700

    add docs

--- pkg/inputs/jetstream_input/jetstream_input.go
+++ pkg/inputs/jetstream_input/jetstream_input.go
@@ -1,4 +1,4 @@
-// © 2022 Nokia.
+// © 2025 Nokia.
 //
 // This code is a Contribution to the gNMIc project (“Work”) made under the Google Software Grant and Corporate Contributor License Agreement (“CLA”) and governed by the Apache License 2.0.
 // No other rights or licenses in or to any of Nokia’s intellectual property are granted for any other purpose.
